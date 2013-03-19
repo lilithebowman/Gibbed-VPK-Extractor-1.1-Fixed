2e0a7d0a58efca6dc92dbd0a6de34e79720fe887
ebowman@gmail.com <ebowman@gmail.com>
2013-03-19 08:02:20 -0400

    Initial Commit
    Fixed a bug with Gibbed's *.vpk Extractor where invalid names in the VPK
    file would cause the program to crash due to an unhandled IO error.

---
+++ Gibbed.Valve.FileFormats/PackageFile.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.InteropServices;
+using Gibbed.Helpers;
+
+namespace Gibbed.Valve.FileFormats
+{
+    public class PackageEntry
+    {
+        public string FileName;
+        public string DirectoryName;
+        public string TypeName;
+        public UInt32 CRC32;
+        public UInt32 Size;
+        public UInt32 Offset;
+        public UInt16 ArchiveIndex;
+        public byte[] SmallData;
+    }
+
+    public class PackageFile
+    {
+        public List<PackageEntry> Entries;
+
+        public void Serialize(Stream output)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Deserialize(Stream input)
+        {
+            if (input.ReadValueU32() != 0x55AA1234)
+            {
+                input.Seek(-4, SeekOrigin.Current);
+            }
+            else
+            {
+                uint version = input.ReadValueU32();
+                uint indexSize = input.ReadValueU32();
+
+                if (version != 1)
+                {
+                    throw new FormatException("unexpected version " + version.ToString());
+                }
+            }
+
+            List<PackageEntry> entries = new List<PackageEntry>();
+
+            // Types
+            while (true)
+            {
+                string typeName = input.ReadStringZ();
+                if (typeName == "")
+                {
+                    break;
+                }
+
+                // Directories
+                while (true)
+                {
+                    string directoryName = input.ReadStringZ();
+                    if (directoryName == "")
+                    {
+                        break;
+                    }
+
+                    // Files
+                    while (true)
+                    {
+                        string fileName = input.ReadStringZ();
+                        if (fileName == "")
+                        {
+                            break;
+                        }
+
+                        PackageEntry entry = new PackageEntry();
+                        entry.FileName = fileName;
+                        entry.DirectoryName = directoryName.Replace("/", "\\");
+                        entry.TypeName = typeName;
+                        entry.CRC32 = input.ReadValueU32();
+                        entry.SmallData = new byte[input.ReadValueU16()];
+                        entry.ArchiveIndex = input.ReadValueU16();
+                        entry.Offset = input.ReadValueU32();
+                        entry.Size = input.ReadValueU32();
+                        
+                        UInt16 terminator = input.ReadValueU16();
+                        if (terminator != 0xFFFF)
+                        {
+                            throw new FormatException("invalid terminator");
+                        }
+
+                        if (entry.SmallData.Length > 0)
+                        {
+                            input.Read(entry.SmallData, 0, entry.SmallData.Length);
+                        }
+
+                        entries.Add(entry);
+                    }
+                }
+            }
+
+            this.Entries = entries;
+        }
+    }
+}
